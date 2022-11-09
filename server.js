ecf4171813b79bf0c36506efb2c737188a1248af
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-08 23:11:40 -0300

    time and bot in messages

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,17 +10,22 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", (socket) => {
-  socket.emit("message", "Welcome to DisCode");
+const bot = "DisCode Bot";
 
-  socket.broadcast.emit("message", "A user has joined the chat");
+io.on("connection", (socket) => {
+  socket.emit("message", formatMessage(bot, "Welcome to DisCode"));
+
+  socket.broadcast.emit(
+    "message",
+    formatMessage(bot, "A user has joined the chat")
+  );
 
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(bot, "A user has left the chat"));
   });
 
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("user", msg));
   });
 });
 
