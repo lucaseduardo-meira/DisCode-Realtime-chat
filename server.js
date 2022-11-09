c46806a8d50c56d815c2c0566febe4c9e177672c
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-08 22:38:33 -0300

    front end update

--- server.js
+++ server.js
@@ -7,10 +7,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname + "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
-  console.log("New socket connection..");
+  socket.emit("message", "Welcome to DisCode");
+
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
