cbc0a4815c5d721fc4ca454504617cfd8201513b
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-08 23:40:14 -0300

    users and room output

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
 
 const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
@@ -39,4 +41,14 @@ function outputMessage(message) {
                         ${message.text}
                     </p>`;
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+function outputUsers(users) {
+  userList.innerHTML = `
+  ${users.map((user) => `<li>${user.username}</li>`).join("")}
+  `;
 }
