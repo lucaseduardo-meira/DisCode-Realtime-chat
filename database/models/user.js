ab456f0f9a210c021fcd0a3f8e13c2ee56080ccf
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-09 23:59:54 -0300

    Create user.js

---
+++ database/models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("../config/index");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+    select: false,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
