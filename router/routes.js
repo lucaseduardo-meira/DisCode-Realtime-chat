8b552bbeb2612e781948bf4515434f72527c0e61
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-10 23:22:57 -0300

    /register page

--- router/routes.js
+++ router/routes.js
@@ -1,8 +1,13 @@
+const path = require("path");
 const express = require("express");
 const route = express.Router();
 
 route.get("/", (req, res) => {
   res.sendFile("public/index.html");
+});
+
+route.get("/register", (req, res) => {
+  res.sendFile(path.resolve("public/register.html"));
 });
 
 module.exports = route;
