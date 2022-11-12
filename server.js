56a3fca7ac846ed967111944b54158b132c6eb88
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-11 23:58:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const bodyParser = require("body-parser");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
@@ -15,6 +16,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const bot = "DisCode Bot";
 
