46568de266be1cc4e5403189b327bc4359f8772e
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-12 00:28:30 -0300

    register page

--- router/routes.js
+++ router/routes.js
@@ -1,22 +1,37 @@
 const path = require("path");
 const express = require("express");
 const route = express.Router();
+const User = require("../database/models/user");
 
 route.get("/", (req, res) => {
   res.sendFile("public/index.html");
 });
-route.post("/", (req, res) => {
-  res.send(req.body);
-  // const { email, password } = req.body;
+route.post("/", async (req, res) => {
+  const { email, password } = req.body;
 
-  // const user = User.findOne({ email }.select("+password"));
+  const user = await User.findOne({ email });
 
-  // if (!user) return res.status(400).send({ error: "Usuário não encontrado" });
-  // console.log(user);
+  if (!user) return res.status(400).send({ error: "Usuário não encontrado" });
+
+  res.json(user);
 });
 
 route.get("/register", (req, res) => {
   res.sendFile(path.resolve("public/register.html"));
+});
+route.post("/register", async (req, res) => {
+  const { email, username } = req.body;
+
+  if (await User.findOne({ email })) {
+    return res.status(400).json({ Erro: "Email já cadastrado" });
+  }
+
+  if (await User.findOne({ username })) {
+    return res.status(400).json({ Erro: "Username já cadastrado" });
+  }
+  const user = await User.create(req.body);
+
+  res.json({ Message: "Usuario criado", User: user });
 });
 
 module.exports = route;
