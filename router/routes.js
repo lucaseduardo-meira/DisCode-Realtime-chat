e72c44edb9640a2c4c7c0540862b44fe76694cd5
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-11 23:56:48 -0300

    Update routes.js

--- router/routes.js
+++ router/routes.js
@@ -5,6 +5,15 @@ const route = express.Router();
 route.get("/", (req, res) => {
   res.sendFile("public/index.html");
 });
+route.post("/", (req, res) => {
+  res.send(req.body);
+  // const { email, password } = req.body;
+
+  // const user = User.findOne({ email }.select("+password"));
+
+  // if (!user) return res.status(400).send({ error: "Usuário não encontrado" });
+  // console.log(user);
+});
 
 route.get("/register", (req, res) => {
   res.sendFile(path.resolve("public/register.html"));
