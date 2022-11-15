5a5c306ec07050ea7d55242b408f79aa55118ed1
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-14 23:40:04 -0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -52,3 +52,16 @@ function outputUsers(users) {
   ${users.map((user) => `<li>${user.username}</li>`).join("")}
   `;
 }
+
+if (window.location.pathname == "/chat") {
+  $onlogout = $("#logout");
+  $onlogout.click(function () {
+    var request = {
+      url: "http://localhost:3000/logout",
+      method: "POST",
+    };
+    $.ajax(request).done(function (response) {
+      location.reload();
+    });
+  });
+}
