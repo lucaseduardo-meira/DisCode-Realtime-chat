e59d3f30295c8dbeea481b990108e23fabf3b2e9
Lucas Eduardo <102125807+lucaseduardo-meira@users.noreply.github.com>
2022-11-19 20:34:34 -0300

    Update index.js

--- database/config/index.js
+++ database/config/index.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv/config");
 
-const mongo_uri = process.env.MONGO_URI;
-
-mongoose.connect(mongo_uri);
+mongoose.connect("mongodb://localhost/DisCode");
 
 module.exports = mongoose;
